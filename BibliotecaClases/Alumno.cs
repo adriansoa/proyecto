d76adde242366874f9df21f545a65da8f5cf962b
Adrian Soa <asoa17@hotmail.com>
2019-04-14 20:16:54 -0400

    add class/Alumno con propiedades/metodos

---
+++ BibliotecaClases/Alumno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaClases
+{
+    public class Alumno : Persona
+    {
+        public DateTime Fecha_Nacimiento {
+             get { return Fecha_Nacimiento; }
+
+            set {
+                 String.Format("{0: dd/MM/yyyy}",Fecha_Nacimiento);
+            }
+        }
+
+
+
+        private String Promocion;
+        
+        public override string ToString()
+        {
+            return string.Format ("Nombre: {0} Nro Doc: {1} Telefono: {2}", this.nombre ,this.nro_documento , this.telefono);
+        }
+
+      
+
+    
+
+        public Alumno()
+        {
+     
+        }
+
+        //Firma de Metodo
+        public void RegistrarAlumno()
+        {   
+        }
+
+
+    }
+}
