6cad9ece92e6b1c245b2c72d19fee16d9b0c6d09
Adrian Soa <asoa17@hotmail.com>
2019-04-14 20:17:30 -0400

    add class/Libro with propiedades/metodos

---
+++ BibliotecaClases/Libro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaClases
+{
+   public class Libro
+    {
+        public int codigo { get; set; }
+        public string titulo { get; set; }
+        public string autor { get; set; }
+        public string editorial { get; set; }
+        public string anho_publicacion { get; set; }
+        public int edicion { get; set; }
+
+
+
+        //Firma de Metodo
+        public void RegistraLibro()
+        {
+        }
+
+        public Libro()
+        {
+
+        }
+
+    }
+}
