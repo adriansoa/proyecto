aa2274eb500127b6862c33513bb00c3645ec0690
Adrian Soa <asoa17@hotmail.com>
2019-04-14 20:22:40 -0400

    add class/Facultad with propiedades/metodos

---
+++ BibliotecaClases/Facultad.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace BibliotecaClases
+{
+    public class Facultad
+    {
+        public int codigo { get; set; }
+        public string Nombre { get; set; }
+        public int anho_fundacion { get; set; }
+
+
+        public Facultad()
+        {
+
+        }
+
+        //Firma de Método
+        public void RegistrarFacultad()
+        {
+
+        }
+
+
+
+    }
+
+
+
+
+}
