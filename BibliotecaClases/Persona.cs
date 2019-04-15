b12eecb40dc3f0cc9f6f0e469846d62eadd15692
Adrian Soa <asoa17@hotmail.com>
2019-04-14 20:22:07 -0400

    add class/Persona with propiedades/metodos

---
+++ BibliotecaClases/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaClases
+{
+   public abstract class Persona
+    {
+
+        public string nro_documento { get; set; }
+        public string tipo_documento { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string fecha_Nacimiento  { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+
+
+
+    }
+}
