0ba791150754e28150cdcc1c4d79600e9212c9eb
Adrian Soa <asoa17@hotmail.com>
2019-05-19 17:46:02 -0400

    creacion form gestion bibliorafia

---
+++ Interfaz_Proyecto_Bibliografia/frmGestionBibliografia.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Interfaz_Proyecto_Bibliografia
+{
+    partial class frmGestionBibliografia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbMateria = new System.Windows.Forms.ComboBox();
+            this.lblBibliografia = new System.Windows.Forms.Label();
+            this.dtgBibliografiaDetalle = new System.Windows.Forms.DataGridView();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgBibliografiaDetalle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Materia";
+            // 
+            // cmbMateria
+            // 
+            this.cmbMateria.FormattingEnabled = true;
+            this.cmbMateria.Location = new System.Drawing.Point(152, 58);
+            this.cmbMateria.Name = "cmbMateria";
+            this.cmbMateria.Size = new System.Drawing.Size(260, 24);
+            this.cmbMateria.TabIndex = 1;
+            // 
+            // lblBibliografia
+            // 
+            this.lblBibliografia.AutoSize = true;
+            this.lblBibliografia.Location = new System.Drawing.Point(70, 137);
+            this.lblBibliografia.Name = "lblBibliografia";
+            this.lblBibliografia.Size = new System.Drawing.Size(78, 17);
+            this.lblBibliografia.TabIndex = 2;
+            this.lblBibliografia.Text = "Bibliografia";
+            // 
+            // dtgBibliografiaDetalle
+            // 
+            this.dtgBibliografiaDetalle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgBibliografiaDetalle.Location = new System.Drawing.Point(73, 187);
+            this.dtgBibliografiaDetalle.Name = "dtgBibliografiaDetalle";
+            this.dtgBibliografiaDetalle.RowTemplate.Height = 24;
+            this.dtgBibliografiaDetalle.Size = new System.Drawing.Size(700, 150);
+            this.dtgBibliografiaDetalle.TabIndex = 3;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(599, 42);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(90, 40);
+            this.btnConsultar.TabIndex = 4;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(226, 412);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(90, 40);
+            this.btnAgregar.TabIndex = 5;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(409, 412);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(90, 40);
+            this.btnEliminar.TabIndex = 6;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(599, 412);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(90, 40);
+            this.btnCerrar.TabIndex = 7;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // frmGestionBibliografia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 508);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.dtgBibliografiaDetalle);
+            this.Controls.Add(this.lblBibliografia);
+            this.Controls.Add(this.cmbMateria);
+            this.Controls.Add(this.label1);
+            this.Name = "frmGestionBibliografia";
+            this.Text = "frmGestionBibliografia";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgBibliografiaDetalle)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbMateria;
+        private System.Windows.Forms.Label lblBibliografia;
+        private System.Windows.Forms.DataGridView dtgBibliografiaDetalle;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
